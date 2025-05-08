a322b3f628ad51c25145cb79289d2dbd16be43c1
tangyubin <tangyubin@tal.com>
2025-05-08 10:43:35 +0800

    init

---
+++ bin/dart_in_learn.dart
@@ -0,0 +1,9 @@
+import 'package:dart_in_learn/dart_in_learn.dart' as dart_in_learn;
+
+void main(List<String> arguments) {
+
+  print('arguments: $arguments');
+
+
+  print('Hello world: ${dart_in_learn.calculate()}!');
+}
