434db7091125579d8b9b717b4e0ec8c5cf67fd47
tangyubin <tangyubin@tal.com>
2025-05-08 14:31:02 +0800

    feat: finish introduction

--- lib/_1_intro.dart
+++ lib/_1_intro.dart
@@ -24,9 +24,17 @@ void intro() {
   
   arr.where((int num) => num.isEven).forEach(print);
   
-  var vigor = Person("vigor", DateTime(2000, 7, 11));
+  var vigor = Person("vigor", DateTime(2000, 7, 11), Color.yellow);
 
   vigor.introduction();
+  
+  var tangthinker = Man('tangthinker', DateTime(2001, 4, 20), Color.yellow, "coding");
+
+  tangthinker.introduction();
+
+  vigor.printWithDelay(Duration(seconds:  3));
+
+  print('this is first than up line');
 
 }
 
@@ -36,24 +44,65 @@ int fibonacci(int n) {
 }
 
 
-class Person {
+class Person extends Model{
   String name;
   DateTime? birthday;
+  Color skin;
+
 
   int? get age => birthday != null ? DateTime.now().year - birthday!.year : 0;
 
-  Person(this.name, this.birthday);
+  Person(this.name, this.birthday, this.skin);
 
-  Person.nonBorn(String name) : this(name, null);
+  Person.nonBorn(String name, Color skin) : this(name, null, skin);
 
+  @override
   void introduction() {
     print('My name is $name');
     print('I born at $birthday');
     if (birthday != null) {
       print('Now I am $age years old');
     }
+    print('And the color of my skin is $skin');
   }
 
+  Future<void> printWithDelay(Duration duration) async {
+    await Future.delayed(duration);
+    introduction();
+  }
+
+}
 
 
+enum Color {
+  red, black, white, yellow
+}
+
+class Man extends Person with Hand{
+  String hobby;
+
+  Man(super.name, DateTime super.birthday, super.skin, this.hobby) {
+    fingerSize = 5;
+  }
+  
+  @override
+  void introduction() {
+    super.introduction();
+    print('I am a man, and My hobby is $hobby');
+    printFingerSize();
+  }
+}
+
+
+mixin Hand {
+  int fingerSize = 5;
+
+  void printFingerSize() {
+    print('And I have $fingerSize finger for each hand');
+  }
+}
+
+
+abstract class Model {
+  void introduction();
 }
